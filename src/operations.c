eb4d781b0927dc7c78c943581b177b143d1b7989
Susana Madriz <139632743+susanamadriz@users.noreply.github.com>
2025-11-11 23:30:48 +0100

    operaciones

--- src/operations.c
+++ src/operations.c
@@ -6,7 +6,48 @@
 /*   By: susanamadriz <susanamadriz@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/02 19:51:11 by sjuan-ma          #+#    #+#             */
-/*   Updated: 2025/11/09 23:09:48 by susanamadri      ###   ########.fr       */
+/*   Updated: 2025/11/11 23:29:59 by susanamadri      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "push_swap.h"
+
+void	sa(t_stack **a)
+{
+	t_stack	*tmp;
+
+	if (!a || !(*a) || !(*a)->next)
+		return ;
+	tmp = (*a)->next;
+	(*a)->next = tmp->next;
+	tmp->next = *a;
+	*a = tmp;
+}
+
+void	sb(t_stack **b)
+{
+	sa(b);
+}
+
+void	ss(t_stack **a, t_stack **b)
+{
+	sa(a);
+	sb(b);
+}
+
+void	pa(t_stack **a, t_stack **b)
+{
+	t_stack	*tmp;
+
+	if (!b || !(*b))
+		return ;
+	tmp = *b;
+	*b = (*b)->next;
+	tmp->next = *a;
+	*a = tmp;
+}
+
+void	pb(t_stack **a, t_stack **b)
+{
+	pa(b, a);
+}
