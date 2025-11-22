cb01d5da7f9e8c0d72acb3454c81e64726bb4167
Susana Madriz <139632743+susanamadriz@users.noreply.github.com>
2025-11-22 19:23:05 +0100

    Actualizar utils.c

--- src/utils.c
+++ src/utils.c
@@ -10,3 +10,20 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "push_swap.h"
+
+int	is_sorted(t_stack *a)
+{
+	while (a && a->next)
+	{
+		if (a->value > a->next->value)
+			return (0);
+		a = a->next;
+	}
+	return (1);
+}
+
+void	print_error(void)
+{
+	write(2, "Error\n", 6);
+}
