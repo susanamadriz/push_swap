5e81087afa7174b39d2e6877e887d04303180850
Susana Madriz <139632743+susanamadriz@users.noreply.github.com>
2025-11-26 10:30:00 +0100

    Actualizando headder

--- include/push_swap.h
+++ include/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: susanamadriz <susanamadriz@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/02 19:51:11 by sjuan-ma          #+#    #+#             */
-/*   Updated: 2025/11/24 23:56:55 by susanamadri      ###   ########.fr       */
+/*   Updated: 2025/11/26 21:09:52 by susanamadri      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 # include <unistd.h>
 # include <stdlib.h>
+# include <limits.h>
 
 typedef struct s_stack
 {
@@ -22,17 +23,41 @@ typedef struct s_stack
 	struct s_stack	*next;
 }	t_stack;
 
-/* main flow */
+/* main */
 void	push_swap(t_stack **a);
 
 /* tiny sorts */
 void	sort_two(t_stack **a);
 void	sort_three(t_stack **a);
 
-/* utils */
-int		is_sorted(t_stack *a);
+/* operations (ya tienes operations.c) */
+void	sa(t_stack **a);
+void	sb(t_stack **b);
+void	ss(t_stack **a, t_stack **b);
+void	pa(t_stack **a, t_stack **b);
+void	pb(t_stack **a, t_stack **b);
+void	ra(t_stack **a);
+void	rb(t_stack **b);
+void	rr(t_stack **a, t_stack **b);
+void	rra(t_stack **a);
+void	rrb(t_stack **b);
+void	rrr(t_stack **a, t_stack **b);
+
+/* stack utils */
 t_stack	*stack_new(int value);
 void	stack_add_back(t_stack **stack, t_stack *new);
+int		stack_size(t_stack *stack);
 void	free_stack(t_stack **stack);
+
+/* validation */
+int		validate_args(int argc, char **argv, t_stack **a);
+int		is_number(const char *s);
+int		is_duplicate(t_stack *a, int n);
+long	ft_atol(const char *str);
+
+/* utils */
+int		is_sorted(t_stack *a);
+void	print_error(void);
+void	free_all(t_stack **a, t_stack **b);
 
 #endif
