0481d1e80b62694b9b69b57c1e9149d6fdafa5c8
Susana Madriz <139632743+susanamadriz@users.noreply.github.com>
2025-11-29 21:56:00 +0100

    Elimino duplicados en utils

--- src/stack_utils.c
+++ src/stack_utils.c
@@ -6,26 +6,19 @@
 /*   By: susanamadriz <susanamadriz@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/02 19:51:11 by sjuan-ma          #+#    #+#             */
-/*   Updated: 2025/11/25 00:00:50 by susanamadri      ###   ########.fr       */
+/*   Updated: 2025/11/29 21:33:43 by susanamadri      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	is_sorted(t_stack *a)
-{
-	while (a && a->next)
-	{
-		if (a->value > a->next->value)
-			return (0);
-		a = a->next;
-	}
-	return (1);
-}
-
 t_stack	*stack_new(int value)
 {
-	t_stack *new = malloc(sizeof(t_stack));
+	t_stack *new;
+
+	new = (t_stack *)malloc(sizeof(t_stack));
+	if (!new)
+		return (NULL);
 	new->value = value;
 	new->next = NULL;
 	return (new);
@@ -33,20 +26,40 @@ t_stack	*stack_new(int value)
 
 void	stack_add_back(t_stack **stack, t_stack *new)
 {
-	t_stack *tmp = *stack;
-	if (!tmp)
+	t_stack *tmp;
+
+	if (!stack || !new)
+		return ;
+	if (!*stack)
 	{
 		*stack = new;
-		return;
+		return ;
 	}
+	tmp = *stack;
 	while (tmp->next)
 		tmp = tmp->next;
 	tmp->next = new;
 }
 
+int	stack_size(t_stack *stack)
+{
+	int	i;
+
+	i = 0;
+	while (stack)
+	{
+		i++;
+		stack = stack->next;
+	}
+	return (i);
+}
+
 void	free_stack(t_stack **stack)
 {
 	t_stack *tmp;
+
+	if (!stack)
+		return ;
 	while (*stack)
 	{
 		tmp = (*stack)->next;
@@ -54,3 +67,4 @@ void	free_stack(t_stack **stack)
 		*stack = tmp;
 	}
 }
+
--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: susanamadriz <susanamadriz@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/02 19:51:11 by sjuan-ma          #+#    #+#             */
-/*   Updated: 2025/11/25 00:05:29 by susanamadri      ###   ########.fr       */
+/*   Updated: 2025/11/29 21:39:40 by susanamadri      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,46 +21,4 @@ int	is_sorted(t_stack *a)
 		a = a->next;
 	}
 	return (1);
-}
-
-t_stack	*stack_new(int value)
-{
-	t_stack *new;
-
-	new = malloc(sizeof(t_stack));
-	if (!new)
-		return (NULL);
-	new->value = value;
-	new->next = NULL;
-	return (new);
-}
-
-void	stack_add_back(t_stack **stack, t_stack *new)
-{
-	t_stack *tmp;
-
-	if (!stack || !new)
-		return;
-
-	if (!(*stack))
-	{
-		*stack = new;
-		return;
-	}
-	tmp = *stack;
-	while (tmp->next)
-		tmp = tmp->next;
-	tmp->next = new;
-}
-
-void	free_stack(t_stack **stack)
-{
-	t_stack *tmp;
-
-	while (*stack)
-	{
-		tmp = (*stack)->next;
-		free(*stack);
-		*stack = tmp;
-	}
 }
